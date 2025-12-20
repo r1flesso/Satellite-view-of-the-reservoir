68017d4504a4a05451e8bd63a2dee4af44de955b
r1flesso <allayarov_tank100@mail.ru>
2025-12-20 22:23:39 +0400

    Update decision_tree_entopy.py

--- src/models/classifical/decision_tree_entopy.py
+++ src/models/classifical/decision_tree_entopy.py
@@ -1,6 +1,7 @@
-from DTC_with_gini import DecisionTreeClassifier_Model
+from decision_tree_gini import DecisionTreeClassifier_Model
 import itertools
 import pandas as pd
+from pathlib import Path
 from sklearn.model_selection import train_test_split
 
 
