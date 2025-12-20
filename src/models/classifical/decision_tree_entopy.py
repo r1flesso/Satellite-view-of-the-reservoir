38c23c7c3303d5291c69d8ac1c6e9c739e5b5a5f
r1flesso <allayarov_tank100@mail.ru>
2025-12-20 22:12:51 +0400

    Create decision_tree_entopy.py

---
+++ src/models/classifical/decision_tree_entopy.py
@@ -0,0 +1,37 @@
+from DTC_with_gini import DecisionTreeClassifier_Model
+import itertools
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+BASE_DIR = Path(__file__).resolve().parents[3]
+DATA_PATH = BASE_DIR / 'data' / 'clear_data.csv'
+
+# Переводим нашу csv таблицу в формат pd.DataFrame
+df = pd.read_csv(DATA_PATH, sep=';', decimal=',')
+
+X = df.drop(columns=['type'])
+y = df['type']
+
+# Разделяем данные на тренировочную, тестовую и валидационную выборки
+X_train, X_val_test, y_train, y_val_test = train_test_split(
+    X, y, train_size=0.7, stratify=y, random_state=42
+)
+
+X_val, X_test, y_val, y_test = train_test_split(
+    X_val_test, y_val_test, test_size=0.66, stratify=y_val_test, random_state=42
+)
+
+
+# Прописываем списки splitters и metrics, по которым будем бежать циклом
+splitters = ['best', 'random']
+metrics = ['balanced_accuracy', 'f1', 'recall', 'precision', 'cohen_kappa', 'matthews_corrcoef']
+
+if __name__ == '__main__':
+    for splitter, metric in itertools.product(splitters, metrics):
+        # Создаем экземпляр класса с критерием 'entropy'
+        Obj = DecisionTreeClassifier_Model(
+            X_train, X_val, X_test, y_train, y_val, y_test, 'entropy', splitter, metric
+        )
+        # Вызываем метод для обучения
+        Obj.objective_optuna()
